ceb04a0698e2eb9e81b8b0e797cc2d2b886e7b29
danny <dpena1094@users.noreply.github.com>
2017-08-05 22:53:32 -0700

    Added mongodb session support for session storage

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var expressValidator = require('express-validator');
 var expressSession = require('express-session');
+var MongoStore = require('connect-mongo')(expressSession);
 var path = require('path');
 var bodyParser = require('body-parser');
 //var compression = require('compression');
@@ -19,12 +20,24 @@ app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(expressValidator());
-app.use(expressSession({ secret: 'K0Ur98j/3yX R~XHH!jmN]LWX/,?DP', saveUninitialized: false, resave: false }));
+app.use(expressSession({
+	secret: 'K0Ur98j/3yX R~XHH!jmN]LWX/,?DP',
+	saveUninitialized: false,
+	resave: false,
+	store: new MongoStore({
+		//ttl: 14 * 24 * 60 * 60, // in seconds
+		url: 'mongodb://localhost/'
+	}),
+	cookie: { maxAge: 180 * 60 * 1000 } // in milliseconds
+}));
 
 app.use('/style', express.static(path.join(__dirname, 'css')));
 app.use('/script', express.static(path.join(__dirname, 'js')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+//app.use(function (request, response, next) {
+	//response.locals.session = request.session;
+//});
 app.use('/', router);
 
 app.listen(port, function () {
